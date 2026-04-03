24a89a3c697ec78b47aa24bfc9f5e0d7f606970f
jacopolegovini <legovini96@gmail.com>
2026-04-03 12:24:13 +0200

    https forced

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('it');
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
